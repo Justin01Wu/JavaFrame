83e9a42ddb0c7cdb55bc775c7df8b5edc3114a21
Justin Wu <justin01.wu@gmail.com>
2014-12-12 11:40:15 -0500

    add comment to HibernateUtil

--- hibernate_h2/src/main/java/com/justin/test/hibernate/HibernateUtil.java
+++ hibernate_h2/src/main/java/com/justin/test/hibernate/HibernateUtil.java
@@ -4,6 +4,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
+/** SessionFactory utils */
 public class HibernateUtil {
 
 	private static final SessionFactory sessionFactory = buildSessionFactory();
