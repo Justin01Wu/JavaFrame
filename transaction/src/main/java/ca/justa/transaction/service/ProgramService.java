cc0e7903ad25ceccdcbd244895096b5e04631b91
Justin Wu <justin01.wu@gmail.com>
2015-08-12 17:51:37 -0400

    improve transaction sample

--- transaction/src/main/java/ca/justa/transaction/service/ProgramService.java
+++ transaction/src/main/java/ca/justa/transaction/service/ProgramService.java
@@ -1,5 +1,12 @@
 package ca.justa.transaction.service;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -8,9 +15,70 @@ import ca.justa.transaction.bean.Program;
 
 public class ProgramService {
 	
+	private DataSource dataSource;
+
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+	
 	@Transactional(propagation = Propagation.SUPPORTS)
-	public Program getProgramById(){
-		return new Program();
+	public void insert(Program program) {
+
+		String sql = "INSERT INTO program (ID, NAME) VALUES (?, ?, ?)";
+		Connection conn = null;
+
+		try {
+			conn = dataSource.getConnection();
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setInt(1, program.getId());
+			ps.setString(2, program.getName());
+			ps.executeUpdate();
+			ps.close();
+
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+	}
+
+	public Program findById(int id) {
+
+		String sql = "SELECT * FROM EMPLOYEE WHERE ID = ?";
+
+		Connection conn = null;
+
+		try {
+			conn = dataSource.getConnection();
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setInt(1, id);
+			Program program = null;
+			ResultSet rs = ps.executeQuery();
+			if (rs.next()) {
+				program = new Program();
+				program.setId(rs.getInt("ID"));
+				program.setName(rs.getString("NAME"));				
+				
+			}
+			rs.close();
+			ps.close();
+			return program;
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
 	}
 
 }
