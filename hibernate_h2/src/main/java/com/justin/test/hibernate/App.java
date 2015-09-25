94c80b0e9213e32f3c36a63d3727b11b24671c5a
Justin Wu <justin01.wu@gmail.com>
2015-09-25 17:21:27 -0400

    improve hibernate sample, add real sql server testing, add one to many example

--- hibernate_h2/src/main/java/com/justin/test/hibernate/App.java
+++ hibernate_h2/src/main/java/com/justin/test/hibernate/App.java
@@ -1,10 +1,14 @@
 package com.justin.test.hibernate;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import com.justin.test.hibernate.bean.Department;
+import com.justin.test.hibernate.bean.Employee;
 
 public class App {
 	
@@ -22,6 +26,11 @@ public class App {
 		entityManager.persist(department);
 		
 		System.out.println(department.getId());
+		
+
+		Employee employee = new Employee("simple employee", department);
+		
+		entityManager.persist(employee);
 		
 		entityManager.getTransaction().commit();
 		
