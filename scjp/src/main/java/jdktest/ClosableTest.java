bddbc9ca3b862fa42413bde365de076048cbda77
Justin Wu <justin01.wu@gmail.com>
2016-07-27 12:05:59 -0400

    improve Closeable sample

--- scjp/src/main/java/jdktest/ClosableTest.java
+++ scjp/src/main/java/jdktest/ClosableTest.java
@@ -3,34 +3,65 @@ package jdktest;
 import java.io.Closeable;
 import java.io.IOException;
 
+class MyClosable implements Closeable {
 
-class MyClosable  implements Closeable {
-	
-	public MyClosable(){}
-	public MyClosable(boolean throwError){
-		if(throwError){
+	private String name;
+
+	public MyClosable(String name) {
+		this.name = name;
+	}
+
+	public MyClosable(boolean throwError) {
+		if (throwError) {
 			throw new RuntimeException("can't create MyClosable");
 		}
 	}
 
 	public void close() throws IOException {
-		System.out.println("MyClosable is closing");		
+		System.out.println("MyClosable is closing " + name);
 	}
-	
+
 }
 
 public class ClosableTest {
-	
-	public static void main(String[] args) throws IOException{
-		
-		try(MyClosable me = new MyClosable()){  // this grammar start since java 1.7
-			System.out.println("do something");
-		}		// this line will automatically close me on async way
-		
 
-		try(MyClosable me = new MyClosable(true)){  // this grammar start since java 1.7
-			System.out.println("do something222");  // this line didn't run because new MyClosable(true) throw exception 
-		}		// this line will NOT close me because me is null
+	public static void main(String[] args) throws IOException {
+
+		try (MyClosable me = new MyClosable("111")) { // this grammar start
+														// since java 1.7
+			System.out.println("do something111");
+		} catch (Exception e) { // this line will automatically close me on
+								// async way
+			e.printStackTrace();
+		}
+
+		System.out.println("\n\r\n\r");
+
+		try (MyClosable me1 = new MyClosable("abc");
+				MyClosable me2 = new MyClosable("def")) { // this grammar start
+															// since java 1.7
+			System.out.println("do something222");
+		} catch (Exception e) { // this line will call me2 firstly, then call me1 firstly
+			e.printStackTrace();
+		}
+
+		System.out.println("\n\r\n\r");
+
+		try (MyClosable me = new MyClosable(true)) { // this grammar start since java 1.7
+			System.out.println("do something333"); // this line didn't run because new MyClosable(true) throw exception
+		} catch (Exception e) { // this line will NOT close me because me is null
+			e.printStackTrace();
+		}
+
+		System.out.println("\n\r\n\r");
+
+		try (MyClosable me1 = new MyClosable("444");
+				MyClosable me2 = new MyClosable(true);) { // this grammar start since java 1.7
+			System.out.println("do something444"); // this line didn't run because new MyClosable(true) throw exception
+
+		} catch (Exception e) { // this line will ONLY close 444 because me2 is null
+			e.printStackTrace();
+		}
 
 	}
 }
