d5e9ce1acc7692149161af3eff5f2c3c63c0f90c
Justin Wu <justin01.wu@gmail.com>
2017-08-11 16:01:28 -0400

    improve generic sample

--- scjp/src/main/java/scjp/testcollection/Generic.java
+++ scjp/src/main/java/scjp/testcollection/Generic.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Generic {
 	
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InstantiationException, IllegalAccessException {
 		List x = new ArrayList();
 		List<?> y = new ArrayList<Integer>();
 		x= y;
@@ -16,9 +16,45 @@ public class Generic {
 		//z= y;  compile error
 		z= x;  // no compile error 
 		
-		List<? extends RuntimeException> aa = new ArrayList< RuntimeException>();
+		List<? extends RuntimeException> aa = new ArrayList<>();
+		// aa.add( new RuntimeException());  // compile error , why?
 		//  aa.add( new IllegalArgumentException());  // compile error , why?
 		
+		List<RuntimeException> bb = new ArrayList<>();
+		bb.add( new RuntimeException());
+		bb.add( new IllegalArgumentException());
+		
+		List<RuntimeException> cc  = getList();
+		cc.add( new RuntimeException());
+		cc.add( new IllegalArgumentException());		
+
+		List<IllegalArgumentException> dd  = getList3(IllegalArgumentException.class);  // this is better way
+		dd.add( new IllegalArgumentException());		
+		
+		//List<RuntimeException> ee  = getList3(IllegalArgumentException.class);  // compile error: RuntimeException didn't match IllegalArgumentException
+		
 	}
+	
+	public static <T extends RuntimeException> List<T> getList(){
+		List<T> aa = new ArrayList<>();
+		//aa.add( new RuntimeException());  // compile error , why?
+		return aa;
+	}
+	
+	public static List<RuntimeException> getList2(){
+		List<RuntimeException> aa = new ArrayList<>();
+		aa.add( new RuntimeException());  
+		return aa;
+	}
+
+	public static <T extends RuntimeException> List<T> getList3(Class<T> type) throws InstantiationException, IllegalAccessException{
+		List<T> aa = new ArrayList<>();
+		T a = type.newInstance();   
+		aa.add( a);  
+		return aa;
+	}
+
+	
+
 
 }
