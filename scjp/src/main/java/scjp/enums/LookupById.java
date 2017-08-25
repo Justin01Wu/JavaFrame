7f98386c7effd3083f90c3364b675483b71ca3b2
Justin Wu <justin01.wu@gmail.com>
2017-08-25 10:23:07 -0400

    add comments for enum.values()

--- scjp/src/main/java/scjp/enums/LookupById.java
+++ scjp/src/main/java/scjp/enums/LookupById.java
@@ -7,7 +7,7 @@ public interface LookupById<E extends Enum<E>> {
 	
 	
 	// Since JDK 1.8, we can add static method on interface
-	// We can declare generic E can extedns multiple class like :  E extends A & B  
+	// We can declare generic E can extends multiple class like :  E extends A & B  
 	public static <E extends Enum<E> & LookupById<E>> E lookupById2(Class<E> enumClass, Integer id){
 			if(id == null) 	return null;
 
@@ -25,6 +25,8 @@ public interface LookupById<E extends Enum<E>> {
 		if(id == null) 	return null;
 
 //		for(E one: E.values()){   // compile error on this line:  The method values() is undefined for the type E
+//                                // values is added by compiler, didn't exist on any java class
+// 								  // see here for details https://stackoverflow.com/questions/13659217/where-is-the-documentation-for-the-values-method-of-enum
 //			if(one.getId() == id){
 //				return one;
 //			}
