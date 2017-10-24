ec0e181574d198414a91170461009f9b6511ac39
Justin Wu <justin01.wu@gmail.com>
2017-10-24 14:44:14 -0400

    add connection session context sample

--- connectionPool/src/main/java/com/justa/test/MsJdbcTest.java
+++ connectionPool/src/main/java/com/justa/test/MsJdbcTest.java
@@ -2,12 +2,13 @@ package com.justa.test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class MsJdbcTest {	
 		
 	    //private static String jdbcUrl = "jdbc:sqlserver://CAVCEXPDB01:1433;databaseName=Vcaps_qa01_9088_20170316";
-		private static String jdbcUrl = "jdbc:sqlserver://CAVCEXPDB01:1433;databaseName=Vcaps_qa01_9088_20170316;user=vcapstest;password=vcapstest"; 
+		private static String jdbcUrl = "jdbc:sqlserver://CAVCDBTST03:1433;databaseName=vcaps_QA_20170430;user=vcapstest;password=vcapstest"; 
 
 		public static void main(String[] args) throws ClassNotFoundException,
 				SQLException {
@@ -19,10 +20,23 @@ public class MsJdbcTest {
 			//Connection con = DriverManager.getConnection(jdbcUrl,"vcapstest", "vcapstest");
 			Connection con = DriverManager.getConnection(jdbcUrl);
 			
+			//setContext(con);
+
 			JtdsJdbcTest.execQuery(con);
 
 			
 			con.close();
+		}
+		
+		private static void setContext(Connection con) throws SQLException{
+			
+			// since sql server 2012, it has sp_set_session_context, which can be used in RLS(Row level security) function 
+			PreparedStatement ps = con.prepareStatement("exec sp_set_session_context @key=N'VCAPSUserId', @value=?");  
+			// call sp_set_session_context to set current user info which will be used by RLS function
+			int userId =  13;
+			ps.setInt(1, userId);
+			boolean success  = ps.execute();
+			
 		}
 
 }
