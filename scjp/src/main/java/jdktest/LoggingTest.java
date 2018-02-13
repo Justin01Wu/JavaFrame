ba2945a1f623e109aa68d19e4b77e9a97b633ac4
Justin Wu <justin01.wu@gmail.com>
2018-02-13 15:23:45 -0500

    improve logging sample

--- scjp/src/main/java/jdktest/LoggingTest.java
+++ scjp/src/main/java/jdktest/LoggingTest.java
@@ -10,12 +10,14 @@ public class LoggingTest {
     public static void main(String[] args) {
     	LOG.info("start main method...");
     	LOG.warning(" a warning...");
-    	LOG.fine(" a fine message...");
+    	LOG.fine(" a fine message...");  // can't see it because by default it only show info level and above message
     	LOG.finer(" a finer message...");
     	
     	Exception x = new RuntimeException("Jutin test exception");
     	
     	LOG.log( Level.SEVERE, x.toString(), x );
+    	
+    	LOG.severe("my severe msg");
     }
 
 }
