9424c3b89ebc042bcf8db6b4cfe474276b89d0e7
Justin01Wu <justin01.wu@gmail.com>
2018-09-20 13:00:50 -0400

    update jwt sample

--- library-test/src/main/java/com/justa/library/test/jjwt/JwtMain.java
+++ library-test/src/main/java/com/justa/library/test/jjwt/JwtMain.java
@@ -1,6 +1,5 @@
 package com.justa.library.test.jjwt;
 
-import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -12,20 +11,31 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.impl.crypto.MacProvider;
 
 
 // comes from https://blog.csdn.net/qq_37636695/article/details/79265711
+
+// please also see https://blog.csdn.net/a975261294/article/details/71937908
 public class JwtMain {
 	
 	public String createJWT(String id, String subject, long ttlMillis) throws Exception {
         SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256; //指定签名的时候使用的签名算法，也就是header那部分，jjwt已经将这部分内容封装好了。
         long nowMillis = System.currentTimeMillis();//生成JWT的时间
         Date now = new Date(nowMillis);
-        Map<String,Object> claims = new HashMap<String,Object>();//创建payload的私有声明（根据特定的业务需要添加，如果要拿这个做验证，一般是需要和jwt的接收方提前沟通好验证方式的）
+        
+        Map<String,Object> claims = new HashMap<String,Object>();
+        //创建payload的私有声明（根据特定的业务需要添加，如果要拿这个做验证，一般是需要和jwt的接收方提前沟通好验证方式的）
         claims.put("uid", "DSSFAWDWADAS...");
         claims.put("user_name", "admin");
         claims.put("nick_name","DASDA121");
+        claims.put("justin","I can add any fields into JWT");
         SecretKey key = generalKey();//生成签名的时候使用的秘钥secret,这个方法本地封装了的，一般可以从本地配置文件中读取，切记这个秘钥不能外露哦。它就是你服务端的私钥，在任何场景都不应该流露出去。一旦客户端得知这个secret, 那就意味着客户端是可以自我签发jwt了。
+        
+        SecretKey key2 = MacProvider.generateKey(); //这里是加密解密的key。
+
+
+        
         //下面就是在为payload添加各种标准声明和私有声明了
         JwtBuilder builder = Jwts.builder() //这里其实就是new一个JwtBuilder，设置jwt的body
                 .setClaims(claims)          //如果有私有声明，一定要先设置这个自己创建的私有的声明，这个是给builder的claim赋值，一旦写在标准的声明赋值之后，就是覆盖了那些标准的声明的
@@ -52,7 +62,8 @@ public class JwtMain {
     }
 	
 	public SecretKey generalKey(){
-        byte[] stringKey = JWTSetting.JWT_SECRET.getBytes();        
+        byte[] stringKey = JWTSetting.JWT_SECRET.getBytes();  
+        // JWT_SECRET will be on server setting on prod, better to add salt to avoid both developer and admin to easily get real key         
         SecretKey key = new SecretKeySpec(stringKey, 0, stringKey.length, "AES");// 根据给定的字节数组使用AES加密算法构造一个密钥，使用 encodedKey中的始于且包含 0 到前 leng 个字节这是当然是所有。（后面的文章中马上回推出讲解Java加密和解密的一些算法）
         return key;
     }
@@ -63,6 +74,8 @@ public class JwtMain {
         System.out.println("  subject: "+ c.getSubject());  //{id:100,name:justin.wu}
         System.out.println("  issuer: " + c.getIssuer());//null
         System.out.println("  uid: " + c.get("uid", String.class));//DSSFAWDWADAS...
+        System.out.println("  justin: " + c.get("justin", String.class));
+        
 		
 	}
 	
