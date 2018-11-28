ed8c7650c34e65142720938a873ad943b8219fd0
Justin01Wu <justin01.wu@gmail.com>
2018-11-28 10:28:03 -0500

    improve jackson sample

--- library-test/src/main/java/com/justa/library/test/jackson/TestJackson.java
+++ library-test/src/main/java/com/justa/library/test/jackson/TestJackson.java
@@ -1,14 +1,25 @@
 package com.justa.library.test.jackson;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+// it comes from https://www.mkyong.com/java/how-to-convert-java-object-to-from-json-jackson/
 public class TestJackson {
-	public static void main(String[] args) throws JsonParseException, JsonMappingException, IOException {
+	public static void main(String[] args) throws JsonParseException, JsonMappingException, IOException {	
+		
+		testSimpleClientFromString();
+		System.out.println();
+		testmanyClientFromFile();
 
+	}
+	
+	private static void testSimpleClientFromString() throws JsonParseException, JsonMappingException, IOException {
 		ObjectMapper mapper = new ObjectMapper();
 
 		// Convert JSON string from file to Object
@@ -17,9 +28,25 @@ public class TestJackson {
 
 		// Convert JSON string to Object
 		String jsonInString = "{\"id\":33,\"name\":\"AIG\",\"dnfReinsurer\":\"justin\"}";
-		Client user1 = mapper.readValue(jsonInString, Client.class);
-		System.out.println(user1);
+		Client client1 = mapper.readValue(jsonInString, Client.class);
+		System.out.println(client1);
+
+	}
+	
+	private static void testmanyClientFromFile() throws JsonParseException, JsonMappingException, IOException {
+		ObjectMapper mapper = new ObjectMapper();
+
+		// Convert JSON string from file to Object
+		InputStream jsonFile = TestJackson.class.getResourceAsStream("/clients.json");
+				
+		JavaType type = mapper.getTypeFactory().constructCollectionType(List.class, Client.class);
+		List <Client> result = mapper.readValue(jsonFile, type);
+		for(Client client:  result) {
+			System.out.println(client);	
+		}
+		
 
 
 	}
+
 }
