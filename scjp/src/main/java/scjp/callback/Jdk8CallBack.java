1375d73585a612e987c703aa6f93bc81c8da488a
Justin01Wu <justin01.wu@gmail.com>
2018-11-28 15:31:34 -0500

    improve callBack function

--- scjp/src/main/java/scjp/callback/Jdk8CallBack.java
+++ scjp/src/main/java/scjp/callback/Jdk8CallBack.java
@@ -6,7 +6,10 @@ public class Jdk8CallBack {
     public static void main(String[] args){
 
     	String msg ="call back print this message11";
-    	callThisWithMessage(msg, (msg2) -> System.out.println("Message received: "+msg2));
+    	
+    	callThisWithMessage(msg, (msg2) -> System.out.println("Message received: "+msg2));    	
+    	// Lambda function, it means creating anonymous OnClickListener with method onClick(msg2){System.out.println("Message received: "+msg2}
+
     }
     
     private static void callThisWithMessage(String msg , OnClickListener callBack) {
