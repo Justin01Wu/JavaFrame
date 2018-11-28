b362415f61b34987a29b72fbe06561f7bff166c7
Justin01Wu <justin01.wu@gmail.com>
2018-11-28 15:49:15 -0500

    improve call back sample

--- scjp/src/main/java/scjp/callback/Jdk8CallBack.java
+++ scjp/src/main/java/scjp/callback/Jdk8CallBack.java
@@ -1,5 +1,10 @@
 package scjp.callback;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Jdk8CallBack {
 	
 	
@@ -9,6 +14,21 @@ public class Jdk8CallBack {
     	
     	callThisWithMessage(msg, (msg2) -> System.out.println("Message received: "+msg2));    	
     	// Lambda function, it means creating anonymous OnClickListener with method onClick(msg2){System.out.println("Message received: "+msg2}
+    	
+    	List<Integer> integers = new ArrayList<>(); 
+    	integers.add(12);
+    	integers.add(35);
+        
+        //convert to comma separated strings
+        String ids = integers.stream().map(Object::toString).collect(Collectors.joining(","));
+        System.out.println(ids);
+        
+        
+        List<Student> list = getStudents(); 
+        Map<Integer, String> map = list.stream().collect(Collectors.toMap(Student::getId, Student::getName));
+        System.out.println(map.get(34));
+        
+
 
     }
     
@@ -16,5 +36,17 @@ public class Jdk8CallBack {
     	callBack.onClick(msg);
     }
 
+    
+    private static List<Student> getStudents(){
+    	Student s;
+    	List<Student> list = new ArrayList<>();
+    	s =  new Student(12,"sdd");
+    	list.add(s);
+    	s =  new Student(34,"g3d7gr");
+    	list.add(s);
+    	
+    	return list;
+    	
+    }
 
 }
