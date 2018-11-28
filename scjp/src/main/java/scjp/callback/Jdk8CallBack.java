5458d9f78692518cbbfe6c235caa7900073cafce
Justin01Wu <justin01.wu@gmail.com>
2018-11-28 16:14:45 -0500

    improve callback sample

--- scjp/src/main/java/scjp/callback/Jdk8CallBack.java
+++ scjp/src/main/java/scjp/callback/Jdk8CallBack.java
@@ -1,8 +1,10 @@
 package scjp.callback;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Jdk8CallBack {
@@ -19,17 +21,30 @@ public class Jdk8CallBack {
     	integers.add(12);
     	integers.add(35);
         
-        //convert to comma separated strings
+        // convert to comma separated strings
         String ids = integers.stream().map(Object::toString).collect(Collectors.joining(","));
         System.out.println(ids);
         
-        
+        // convert list to map on Collectors.toMap
         List<Student> list = getStudents(); 
         Map<Integer, String> map = list.stream().collect(Collectors.toMap(Student::getId, Student::getName));
         System.out.println(map.get(34));
         
+        // convert list to map2 on forEach    
+        Map<Integer, Student> map2 = new HashMap<>();
+        list.forEach(e->map2.put(e.getId(), e));
+        System.out.println(map2.get(34).getName());
+        
+        // convert list to map2 on Collectors.toMap for itself   
+        Map<Integer, Student> map3 = list.stream().collect(Collectors.toMap(Student::getId, student -> student));
+        System.out.println(map3.get(34).getName());
+        
+        // convert list to map2 on Collectors.toMap for itself 
+        Map<Integer, Student> map4 = list.stream().collect(Collectors.toMap(Student::getId, Function.identity()));
+        System.out.println(map4.get(34).getName());
+        
 
-
+        
     }
     
     private static void callThisWithMessage(String msg , OnClickListener callBack) {
