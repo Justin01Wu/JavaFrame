b3d55443e0b134c780f406618be431161e44811d
Justin01wu <Justin01.wu@gmail.com>
2019-11-03 12:05:39 -0500

    improve lambda VerifyNewS3Object

--- aws-lambda/src/main/java/com/justa/test/aws/message/VerifyNewS3Object.java
+++ aws-lambda/src/main/java/com/justa/test/aws/message/VerifyNewS3Object.java
@@ -26,6 +26,7 @@ public class VerifyNewS3Object implements RequestHandler<Map<String, Object>,Str
     private final static AmazonSNS snsClient = AmazonSNSClientBuilder.defaultClient();
     
     private static String topicArn = System.getenv("SNS_TOPIC_ARN");  //String topicArn = "arn:aws:sns:us-east-1:137200312110:MyTopic";
+    private static String debug = System.getenv("debug");
 	
 	@Override
 	public String handleRequest(Map<String, Object> input, Context context) {		
@@ -39,8 +40,8 @@ public class VerifyNewS3Object implements RequestHandler<Map<String, Object>,Str
 			        "Records": [
 			            */        
 			                    
-		System.out.println("input type: " + input.getClass().getName());
-		System.out.println("input value: " + input);
+		System.out.println("    input type: " + input.getClass().getName());
+		System.out.println("    input value: " + input);
 		List<?> list = (List<?>)input.get("Records");
 		Map<String, Object> one = (Map<String, Object>)(list.get(0));
 		String json = one.get("body").toString();
@@ -53,48 +54,107 @@ public class VerifyNewS3Object implements RequestHandler<Map<String, Object>,Str
 	}
 	
 	static void handleInput(String json) throws IOException{
-		System.out.println("input value: " + json);
+		System.out.println("    input value: " + json);
 		
 		Object document = Configuration.defaultConfiguration().jsonProvider().parse(json);
 		
 		String bucketName = JsonPath.read(document, "$.Records[0].s3.bucket.name");
 		String objectName = JsonPath.read(document, "$.Records[0].s3.object.key");
 		
-		System.out.println("bucketName: " + bucketName);
-		System.out.println("objectName: " + objectName);
+		System.out.println("    bucketName: " + bucketName);
+		System.out.println("    objectName: " + objectName);
 		if(!objectName.endsWith(".prere.txt")){
-			String msg = "file name must end with .prere.txt";
-			if(topicArn == null){
-				System.out.println("Didn't find Environment variable SNS_TOPIC_ARN, so do nothing");
-				System.out.println(msg);
-				return ;
-			}else{
-				PublishRequest publishRequest = new PublishRequest(topicArn, msg);
-				PublishResult publishResponse = snsClient.publish(publishRequest);
-				System.out.println("MessageId: " + publishResponse.getMessageId());
-			}
+			String msg = "    file name must end with .prere.txt";
+			writeMsg(msg);
 		}
 		
 		S3Object fullObject = s3Client.getObject(new GetObjectRequest(bucketName, objectName));
 		
         InputStreamReader isr3 = new InputStreamReader(fullObject.getObjectContent());
-        displayTextInputStream(isr3);
+        String msg = verifyTextInputStream(isr3);
+        if(msg != null){
+        	writeMsg(msg);
+        }
 		
 		
 	}
 	
-    private static void displayTextInputStream(InputStreamReader isr) throws IOException {
+    static String verifyTextInputStream(InputStreamReader isr) throws IOException {
         // Read the text input stream one line at a time and display each line.
         BufferedReader reader = new BufferedReader(isr);
         String line = null;
         int rows = 0;
         int size = 0;
+        String msg;
         while ((line = reader.readLine()) != null) {
-            //System.out.println(line);
+
         	size = size + line.length();  
             rows++;
-           	System.out.println(line);
+            
+           	if(rows%1000 == 1){
+           		System.out.println("    rows = " + rows +", size = " + size);
+           		System.out.println("    " + line);           		
+           	}
+           	
+            String[] fields = line.split("\t");
+            if(fields.length != 5){
+            	msg = "    expected 5 fields, but not at row " + rows;
+            	msg = msg + "\r\n      : " + line;
+            	return msg;
+            }
+           	
+            if(rows>1){
+            	// IterId	EVENTID	SeqId	LOSS	riskGroup
+            	//27	600000002	2013-09-06 21:22:00	13591155.1216389	EuropeEQ
+            	if(!verifyInteger(fields[0])){
+            		msg = "    expected integer on field 1 at row " + rows+":" ;
+            		msg = msg + "\r\n      : " + line;
+            		return msg;
+            	}
+            	if(!verifyInteger(fields[1])){
+            		msg = "    expected integer on field 2 at row " + rows+":" ;
+            		msg = msg + "\r\n      : " + line;
+            		return msg;
+            	}
+            	if(!verifyDouble(fields[3])){
+            		msg = "    expected integer on field 4 at row " + rows+":" ;
+            		msg = msg + "\r\n      : " + line;
+            		return msg;
+            	}
+            }           	
+
         }
-        System.out.println("rows = " + rows +", size = " + size);
+        return null;
+        
+    }
+    
+    private static void writeMsg(String msg){
+		if(topicArn == null){
+			System.out.println("    Didn't find Environment variable SNS_TOPIC_ARN, so do nothing");
+			System.out.println(msg);
+			return ;
+		}else{
+			PublishRequest publishRequest = new PublishRequest(topicArn, msg);
+			PublishResult publishResponse = snsClient.publish(publishRequest);
+			System.out.println("    MessageId: " + publishResponse.getMessageId());
+		}
+    }
+    
+    private static boolean verifyInteger(String value){
+    	try{
+    		Integer.parseInt(value);
+    		return true;
+    	}catch(NumberFormatException e){
+    		return false;
+    	}
+    }
+    
+    private static boolean verifyDouble(String value){
+    	try{
+    		Double.parseDouble(value);
+    		return true;
+    	}catch(NumberFormatException e){
+    		return false;
+    	}
     }
 }
--- aws-lambda/src/test/java/com/justa/test/aws/message/VerifyNewS3ObjectTest.java
+++ aws-lambda/src/test/java/com/justa/test/aws/message/VerifyNewS3ObjectTest.java
@@ -1,10 +1,10 @@
 package com.justa.test.aws.message;
 
-import static org.junit.Assert.fail;
-
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URISyntaxException;
 import java.util.Scanner;
 
@@ -25,12 +25,24 @@ public class VerifyNewS3ObjectTest {
 	}
 
 	@Test
-	public void test() throws URISyntaxException, IOException {
-		String sample = VerifyNewS3ObjectTest.class.getSimpleName() + ".json";
+	public void testHandleInput() throws URISyntaxException, IOException {
+		String fileName = VerifyNewS3ObjectTest.class.getSimpleName() + ".json";
 		
-		String json = readJSONFile(sample);
+		String json = readJSONFile(fileName);
 		
 		VerifyNewS3Object.handleInput(json);
+	}
+	
+	@Test
+	public void testVerifyTextInputStream() throws URISyntaxException, IOException {
+		String fileName = VerifyNewS3ObjectTest.class.getSimpleName() + ".prere.txt";
+		File targetFile = new File("src\\test\\resources\\" + fileName);
+		FileInputStream fin=new FileInputStream(targetFile);
+		InputStreamReader reader =  new InputStreamReader(fin);
+		String msg = VerifyNewS3Object.verifyTextInputStream(reader);
+		if(msg != null){
+			System.out.println(msg);
+		}
 	}
 
 }
