74ad4ebcca9e60f22662701576332c76202e3791
Justin01wu <Justin01.wu@gmail.com>
2019-11-26 20:33:11 -0500

    add AWS KMS sample

--- aws/src/main/java/com/justa/test/aws/manage/ListKmsKey.java
+++ aws/src/main/java/com/justa/test/aws/manage/ListKmsKey.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 
 import com.amazonaws.services.kms.AWSKMS;
 import com.amazonaws.services.kms.AWSKMSClientBuilder;
+import com.amazonaws.services.kms.model.DescribeKeyRequest;
+import com.amazonaws.services.kms.model.DescribeKeyResult;
 import com.amazonaws.services.kms.model.KeyListEntry;
 import com.amazonaws.services.kms.model.ListKeysResult;
 
@@ -19,6 +21,14 @@ public class ListKmsKey {
 		
 		for(KeyListEntry entry : result.getKeys()) {
 			System.out.println(entry);	
+			DescribeKeyRequest req = new DescribeKeyRequest().withKeyId(entry.getKeyId());
+			DescribeKeyResult dResult = kmsClient.describeKey(req);
+			System.out.println("state = "  + dResult.getKeyMetadata().getKeyState());	
+			System.out.println("desc = "  + dResult.getKeyMetadata().getDescription());
+			System.out.println("usage = "  + dResult.getKeyMetadata().getKeyUsage());
+			System.out.println("manager = "  + dResult.getKeyMetadata().getKeyManager());
+			System.out.println("enabled = "  + dResult.getKeyMetadata().getEnabled());
+			System.out.println("created = "  + dResult.getKeyMetadata().getCreationDate());
 		}
 		
 
