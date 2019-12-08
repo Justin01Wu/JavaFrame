02bbdd5d61fef98c36a1c1c63dd2307978d0bf6a
Justin01wu <Justin01.wu@gmail.com>
2019-12-08 18:34:58 -0500

    improve GeneralLambda.java

--- aws-lambda/src/main/java/com/justa/test/aws/message/VerifyNewS3Object.java
+++ aws-lambda/src/main/java/com/justa/test/aws/message/VerifyNewS3Object.java
@@ -54,44 +54,12 @@ public class VerifyNewS3Object implements RequestHandler<Map<String, Object>,Str
 			e.printStackTrace();
 		}
 		
-		getSysteInfo();
+
 		
 		return null;
 	}
 	
-	static void getSysteInfo() {
-		
-		NumberFormat f = NumberFormat.getInstance();		
-		
-		System.out.println("==>Java version = " +  System.getProperty("java.version"));
-		System.out.println("==>java.vm.version = " +  System.getProperty("java.vm.version"));		
-		System.out.println("==>java.vendor = " +  System.getProperty("java.vendor"));
-		System.out.println("==>java.vm.vendor = " +  System.getProperty("java.vm.vendor"));
-		System.out.println("==>java.vm.name = " +  System.getProperty("java.vm.name"));			 
-		System.out.println("==>Java home = " +  System.getProperty("java.home"));
-		
-		System.out.println("==>Os name = " +  System.getProperty("os.name"));
-		System.out.println("==>Os version = " +  System.getProperty("os.version"));
-		System.out.println("==>user.dir = " +  System.getProperty("user.dir"));
-		System.out.println("==>user.name = " +  System.getProperty("user.name"));
-		System.out.println("==>user.home = " +  System.getProperty("user.home"));
-		
-		long df = new File("/").getFreeSpace();
-		long dt = new File("/").getTotalSpace();
-		System.out.println("==>free disk = " + f.format(df / 1024));
-		System.out.println("==>total disk = " + f.format(dt / 1024) );
 
-		Runtime runtime = Runtime.getRuntime();
-		long maxMemory = runtime.maxMemory();
-		long allocatedMemory = runtime.totalMemory();
-		long freeMemory = runtime.freeMemory();
-		System.out.println("==>free memory: " + f.format(freeMemory / 1024) );
-		System.out.println("==>allocated memory: " + f.format(allocatedMemory / 1024) );
-		System.out.println("==>max memory: " + f.format(maxMemory / 1024) );
-		System.out.println("==>total free memory: " + f.format((freeMemory + (maxMemory - allocatedMemory))));
-		
-		
-	}
 	
 	static void handleInput(String jsonStr) throws IOException, JMSException{
 		
