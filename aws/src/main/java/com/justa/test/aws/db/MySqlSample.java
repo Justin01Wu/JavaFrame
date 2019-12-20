a69ed3207df1bcfd9a154dc79a6a2bec089fa004
Justin01wu <Justin01.wu@gmail.com>
2019-12-19 22:53:00 -0500

    improve MySqlSample.java

--- aws/src/main/java/com/justa/test/aws/db/MySqlSample.java
+++ aws/src/main/java/com/justa/test/aws/db/MySqlSample.java
@@ -8,7 +8,8 @@ import java.sql.Statement;
 
 public class MySqlSample {
 
-	private static String serverUrl = "localhost:3306";
+	private static String ServerName = "localhost";
+	private static String PORT = "3306";
 	private static String DB = "testDb";
 	private static String User = "admin";
 	private static String Password = "admin123";
@@ -17,11 +18,14 @@ public class MySqlSample {
 	public static void main(String args[]) throws ClassNotFoundException, SQLException {
 
 		String serverName = System.getenv("ServerName");
-		String port = System.getenv("Port");
-
-		if (serverName != null && port != null) {
-			serverUrl = serverName + ":" + port;
+		if (serverName != null ) {
+			ServerName = serverName ;
 		}
+		String port = System.getenv("Port");
+		if (port != null ) {
+			PORT = port ;
+		}
+
 		String db = System.getenv("DB");
 		if (db != null ) {
 			DB=db;
@@ -31,21 +35,21 @@ public class MySqlSample {
 			User=user;
 		}
 		String password = System.getenv("Password");
-		if (db != null ) {
+		if (password != null ) {
 			Password=password;
 		}
 		
-		String dbUrl = "jdbc:mysql://"+ serverUrl + "/"+ DB;
+		String dbUrl = "jdbc:mysql://"+ ServerName + ":" + PORT + "/"+ DB;
 		System.out.println(dbUrl);
 		Class.forName("com.mysql.jdbc.Driver");
 		Connection con = DriverManager.getConnection(dbUrl, User, Password);		
 
 		
 		Statement stmt = con.createStatement();
-		stmt.executeUpdate("CREATE TABLE Human ( ID int NOT NULL, name varchar(255) NOT NULL, PRIMARY KEY (ID) )");
-		stmt.executeUpdate("insert into Human values(1, 'Justin')");
-		stmt.executeUpdate("insert into Human values(3, 'Rita')");
-		ResultSet rs = stmt.executeQuery("select * from Human )");
+//		stmt.executeUpdate("CREATE TABLE Human ( ID int NOT NULL, name varchar(255) NOT NULL, PRIMARY KEY (ID) )");
+//		stmt.executeUpdate("insert into Human values(1, 'Justin')");
+//		stmt.executeUpdate("insert into Human values(3, 'Rita')");
+		ResultSet rs = stmt.executeQuery("select * from Human ");
 		while (rs.next())
 			System.out.println(rs.getInt("Id") + "  " + rs.getString("name") );
 		con.close();
