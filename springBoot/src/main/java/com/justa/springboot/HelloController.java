a735bcab2518de9ea734479251489cede6113757
Justin.Wu <justin.wu@validusholdings.com>
2020-01-07 17:01:57 -0500

    improve springBoot with producer

--- springBoot/src/main/java/com/justa/springboot/HelloController.java
+++ springBoot/src/main/java/com/justa/springboot/HelloController.java
@@ -1,7 +1,8 @@
 package com.justa.springboot;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 // from https://spring.io/guides/gs/serving-web-content/
 @RestController
@@ -13,8 +14,17 @@ public class HelloController {
   }
   
   @RequestMapping("/greeting2")
+  //@ResponseBody
   public Greeting greeting2() {
     return new Greeting(1, "greeting2");
+ 
+  }
+  
+  //@RequestMapping(value="/greeting3", produces={"application/json","application/xml"})  
+  @RequestMapping(value="/greeting3", produces={"application/json"})
+  // have to use produces to set return content-type, but it can be dynamically to match request content-type if it is a list  
+  public String greeting3() {
+    return "{\"msg\": \"greeting3\"}";
  
   }
 
