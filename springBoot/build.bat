a4fef6a2d5f7521f01051fd7a30b7dd19e080af1
Justin.Wu <justin.wu@validusholdings.com>
2020-01-07 11:40:41 -0500

    add springBoot sample

---
+++ springBoot/build.bat
@@ -0,0 +1,7 @@
+
+
+set JAVA_HOME=C:\_program\amazon-corretto-11.0.4.11.1-windows-x64\jdk11.0.4_10
+set path=%JAVA_HOME%\bin;%path%
+
+mvn clean package -Dmaven.test.skip=true
+rem mvn spring-boot:run
