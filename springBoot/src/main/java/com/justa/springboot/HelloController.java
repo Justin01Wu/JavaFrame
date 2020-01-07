aabc8b8f81431fd5bf0a591b95c45bd23d7e3038
Justin.Wu <justin.wu@validusholdings.com>
2020-01-07 14:03:58 -0500

    Add a Home Page

--- springBoot/src/main/java/com/justa/springboot/HelloController.java
+++ springBoot/src/main/java/com/justa/springboot/HelloController.java
@@ -3,10 +3,11 @@ package com.justa.springboot;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+// from https://spring.io/guides/gs/serving-web-content/
 @RestController
 public class HelloController {
 
-  @RequestMapping("/")
+  @RequestMapping("/greeting")
   public String index() {
     return "Greetings from Spring Boot!";
   }
