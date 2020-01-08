dd93e55fe28b667dae5bbf82a02efbf71b2bbe3a
Justin01wu <Justin01.wu@gmail.com>
2020-01-07 20:23:22 -0500

    improve springboot sample

--- springBoot/src/main/java/com/justa/springboot/HelloController.java
+++ springBoot/src/main/java/com/justa/springboot/HelloController.java
@@ -1,6 +1,5 @@
 package com.justa.springboot;
 
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,7 +23,7 @@ public class HelloController {
   @RequestMapping(value="/greeting3", produces={"application/json"})
   // have to use produces to set return content-type, but it can be dynamically to match request content-type if it is a list  
   public String greeting3() {
-    return "{\"msg\": \"greeting3\"}";
+    return "{\"msg\": \"greeting 3\"}";
  
   }
 
