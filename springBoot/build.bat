1d7ea16ce722c635535738167c2045f0d65e8a92
Justin01wu <Justin01.wu@gmail.com>
2020-01-07 21:21:52 -0500

    improve springboot sample

--- springBoot/build.bat
+++ springBoot/build.bat
@@ -1,7 +1,7 @@
 
 
-set JAVA_HOME=C:\_program\amazon-corretto-11.0.4.11.1-windows-x64\jdk11.0.4_10
-set path=%JAVA_HOME%\bin;%path%
+rem set JAVA_HOME=C:\_program\amazon-corretto-11.0.4.11.1-windows-x64\jdk11.0.4_10
+rem set path=%JAVA_HOME%\bin;%path%
 
 mvn clean package -Dmaven.test.skip=true
 rem mvn spring-boot:run
--- springBoot/run.bat
+++ springBoot/run.bat
@@ -1,4 +1,4 @@
-set JAVA_HOME=C:\_program\amazon-corretto-11.0.4.11.1-windows-x64\jdk11.0.4_10
-set path=%JAVA_HOME%\bin;%path%
+rem set JAVA_HOME=C:\_program\amazon-corretto-11.0.4.11.1-windows-x64\jdk11.0.4_10
+rem set path=%JAVA_HOME%\bin;%path%
 
 mvn spring-boot:run
