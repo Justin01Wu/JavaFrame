bfe11a541b73529570b7a64a404a393d63041d5d
Justin.Wu <justin.wu@validusholdings.com>
2020-02-05 15:32:53 -0500

    add Eclipse tips on AccessPrivateVariable.java for instance variable break point

--- scjp/src/main/java/scjp/testclass/AccessPrivateVariable.java
+++ scjp/src/main/java/scjp/testclass/AccessPrivateVariable.java
@@ -5,40 +5,45 @@ An object can accesses the private variables of another object
 
 An object can [NOT] accesses the private variables of another object
  from the [different] class.
+ 
+IDE can directly set breakpoint on instance variable definition: 
+   any read or write access from anywhere will stop for it 
 
 */
 package scjp.testclass;
 
 public class AccessPrivateVariable {
-    private int x;
-    public AccessPrivateVariable(int x) {
-        this.x=x;
-    }
+	private int x;
 
-    public int getX(AccessPrivateVariable another){
-        return another.x;
-    }
+	public AccessPrivateVariable(int x) {
+		this.x = x;
+	}
 
+	public int getX(AccessPrivateVariable another) {
+		return another.x;
+	}
 
-        public static void main(String[] args) {
-            AccessPrivateVariable a = new AccessPrivateVariable(12);
-            AccessPrivateVariable b = new AccessPrivateVariable(34);
-            AnotherClass c=new AnotherClass(56);
-            System.out.println(b.getX(a));
-            System.out.println(c.getX(a));
+	public static void main(String[] args) {
+		
+		AccessPrivateVariable a = new AccessPrivateVariable(12);
+		AccessPrivateVariable b = new AccessPrivateVariable(34);		
+		System.out.println(b.getX(a));  // actually it return a.x, not itself.x
+		
+		AnotherClass c = new AnotherClass(56);
+		System.out.println(c.getY(a));
 
-        }
+	}
 }
 
+class AnotherClass {
+	private int y;
 
-    class AnotherClass{
-        private int x;
-        public AnotherClass(int x){
-            this.x=x;
-        }
+	public AnotherClass(int y) {
+		this.y = y;
+	}
 
-        public int getX(AccessPrivateVariable another){
-            //return another.x;  // This can't be complied because this is
-            return 0;
-        }
-    }
+	public int getY(AccessPrivateVariable another) {
+		// return another.x; // This can't be complied because this is
+		return y;
+	}
+}
