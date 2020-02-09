0548c0f746727f0b70d9a06d0d7fa0e611b8ce9b
Justin01wu <Justin01.wu@gmail.com>
2020-02-09 11:14:06 -0500

    add logging into springBoot Application.java

--- springBoot/src/main/java/com/justa/springboot/Application.java
+++ springBoot/src/main/java/com/justa/springboot/Application.java
@@ -2,6 +2,8 @@ package com.justa.springboot;
 
 import java.util.Arrays;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,8 +14,12 @@ import org.springframework.context.annotation.Bean;
 // from https://spring.io/guides/gs/spring-boot/
 @SpringBootApplication
 public class Application {
+	
+	private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
   public static void main(String[] args) {
+	  
+	  logger.info("    ==>> application started from here...");
     SpringApplication.run(Application.class, args);
     
     // please see WebSecurityConfig for how to pass token check 
