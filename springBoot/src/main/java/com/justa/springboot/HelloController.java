faa7429b7d3df37603a6f01d302995354516216b
Justin.Wu <justin.wu@validusholdings.com>
2020-02-11 11:53:11 -0500

    improve actuator on spring boot: add customized metrics

--- springBoot/src/main/java/com/justa/springboot/HelloController.java
+++ springBoot/src/main/java/com/justa/springboot/HelloController.java
@@ -1,11 +1,30 @@
 package com.justa.springboot;
 
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 
 // from https://spring.io/guides/gs/serving-web-content/
 @RestController
 public class HelloController {
+	
+	private MeterRegistry meterRegistry;
+	private Counter myCounter;
+	
+	@Autowired
+	public HelloController(	MeterRegistry meterRegistry) {
+		this.meterRegistry = meterRegistry;
+		
+		myCounter = Counter    .builder("greeting3Counter")
+		        .description("indicates called count of the greeting3 api")
+		        .tags("dev", "performance")
+		        .register(meterRegistry);
+	}
 
   @RequestMapping("/api/greeting")
   public String greeting() {
@@ -23,7 +42,12 @@ public class HelloController {
   @RequestMapping(value="/api/greeting3", produces={"application/json"})
   // have to use produces to set return content-type, but it can be dynamically to match request content-type if it is a list  
   public String greeting3() {
-    return "{\"msg\": \"greeting 3\"}";
+	  
+	  // customized metrics for actuator
+	  myCounter.increment();
+	  meterRegistry.gauge("greeting3_timeStamp", new Date().getTime());
+	  
+	  return "{\"msg\": \"greeting 3\"}";
  
   }
 
