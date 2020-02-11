4dc42dd1e78d865a4284fd54217ab16f14eaebad
Justin.Wu <justin.wu@validusholdings.com>
2020-02-11 13:54:58 -0500

    add customized data type for lambda

--- aws/src/main/java/com/justa/test/aws/lambda/GeneralFunctionService.java
+++ aws/src/main/java/com/justa/test/aws/lambda/GeneralFunctionService.java
@@ -3,9 +3,9 @@ package com.justa.test.aws.lambda;
 import com.amazonaws.services.lambda.invoke.LambdaFunction;
 import com.justa.test.aws.message.MyDataDto;
 
-public interface GeneralLambdaService {
+public interface GeneralFunctionService {
 	
 	@LambdaFunction(functionName = "GeneralFunction:GeneralFunctionTest")  // use alias to avoid version affection
-	MyDataDto callGeneralLambda(MyDataDto input);
-	// this interface parameters should match GeneralLambda.handleRequest parameters
+	MyDataDto callGeneralFunction(MyDataDto input);
+	// this interface parameters should match GeneralFunction.handleRequest parameters
 }
--- aws/src/main/java/com/justa/test/aws/lambda/ManuallyCallLambda.java
+++ aws/src/main/java/com/justa/test/aws/lambda/ManuallyCallLambda.java
@@ -7,16 +7,17 @@ import com.justa.test.aws.message.MyDataDto;
 public class ManuallyCallLambda {
 	public static void main(String args[]) throws Exception {
 
-		GeneralLambdaService generalLambdaService = LambdaInvokerFactory
+		GeneralFunctionService generalFunctionService = LambdaInvokerFactory
 				.builder()
 				.lambdaClient(AWSLambdaClientBuilder.defaultClient())
-				.build(GeneralLambdaService.class);
+				.build(GeneralFunctionService.class);
 
 		
 		for(int i=0; i<10; i++) {
 			MyDataDto dto =  new MyDataDto();
 			dto.setId(i);
-			MyDataDto output = generalLambdaService.callGeneralLambda(dto);
+			
+			MyDataDto output = generalFunctionService.callGeneralFunction(dto);
 			System.out.println(output.getCreatedAt());
 		}
 
