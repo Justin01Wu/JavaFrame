9f5c53f03da7fa2448c4400278679c400d5621fc
Justin01wu <Justin01.wu@gmail.com>
2020-02-11 22:50:01 -0500

    add AsyncCallLambda.java

---
+++ aws/src/main/java/com/justa/test/aws/lambda/AsyncCallLambda.java
@@ -0,0 +1,36 @@
+package com.justa.test.aws.lambda;
+
+import java.nio.ByteBuffer;
+
+import com.amazonaws.services.lambda.AWSLambdaAsync;
+import com.amazonaws.services.lambda.AWSLambdaAsyncClientBuilder;
+import com.amazonaws.services.lambda.model.InvokeRequest;
+import com.amazonaws.services.lambda.model.InvokeResult;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.justa.test.aws.message.MyDataDto;
+
+// from https://stackoverflow.com/questions/31714788/can-an-aws-lambda-function-call-another?rq=1
+public class AsyncCallLambda {
+	
+	public static void main(String args[]) throws Exception {
+		AWSLambdaAsync awsLambdaAsync = AWSLambdaAsyncClientBuilder.standard().build();
+
+		InvokeRequest invokeRequest = new InvokeRequest();
+		
+		MyDataDto dto =  new MyDataDto();
+		dto.setId(1212);
+		
+		ObjectMapper Obj = new ObjectMapper(); 
+		  
+        String jsonStr = Obj.writeValueAsString(dto); 
+        
+        ByteBuffer payload = ByteBuffer.wrap(jsonStr.getBytes());
+		
+		invokeRequest.withFunctionName("GeneralFunction:GeneralFunctionTest").withPayload(payload);
+
+		InvokeResult invokeResult = awsLambdaAsync.invoke(invokeRequest); 
+		String result = new String(invokeResult.getPayload().array());
+		System.out.println(result);
+	}
+
+}
