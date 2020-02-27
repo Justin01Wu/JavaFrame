b2d7caf8acd339e7173d3c3264c8d2c5e2e8e783
Justin.Wu <justin.wu@validusholdings.com>
2020-02-27 16:01:20 -0500

    add EnumConverter sample

--- springBoot/src/main/java/com/justa/springboot/db/PositionEnumConverter.java
+++ springBoot/src/main/java/com/justa/springboot/db/PositionEnumConverter.java
@@ -3,6 +3,8 @@ package com.justa.springboot.db;
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 
+// from https://thoughts-on-java.org/jpa-21-type-converter-better-way-to/
+
 @Converter(autoApply = true)   // tell the JPA provider to use it to map PositionEnum in any entity
 public class PositionEnumConverter implements AttributeConverter<PositionEnum, String> {
  
