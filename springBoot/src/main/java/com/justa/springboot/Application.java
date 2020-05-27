1674b3d6fde453707fe6005cd5797dad959e0311
Justin.Wu <justin.wu@validusholdings.com>
2020-05-27 14:23:21 -0400

    add @PostConstruct sample

--- springBoot/src/main/java/com/justa/springboot/Application.java
+++ springBoot/src/main/java/com/justa/springboot/Application.java
@@ -30,11 +30,11 @@ public class Application {
 
 			logger.info("Let's inspect the beans provided by Spring Boot:");
 
-			String[] beanNames = ctx.getBeanDefinitionNames();
-			Arrays.sort(beanNames);
-			for (String beanName : beanNames) {
-				System.out.println(beanName);
-			}
+//			String[] beanNames = ctx.getBeanDefinitionNames();
+//			Arrays.sort(beanNames);
+//			for (String beanName : beanNames) {
+//				System.out.println(beanName);
+//			}
 
 		};
 	}
