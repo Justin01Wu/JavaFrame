761b533de015a0fc822908374739e3656f6a11a4
Justin.Wu <justin.wu@validusholdings.com>
2020-05-27 14:20:29 -0400

    add @PostConstruct sample

--- springBoot/src/main/java/com/justa/springboot/Application.java
+++ springBoot/src/main/java/com/justa/springboot/Application.java
@@ -10,34 +10,33 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-
 // from https://spring.io/guides/gs/spring-boot/
 @SpringBootApplication
 public class Application {
-		
+
 	private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) {
-	  
+
 		logger.info("    ==>> application started from here...");
-	  
+
 		SpringApplication.run(Application.class, args);
-    
-  }
 
-  @Bean
-  public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-    return args -> {
+	}
 
-      System.out.println("Let's inspect the beans provided by Spring Boot:");
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
 
-      String[] beanNames = ctx.getBeanDefinitionNames();
-      Arrays.sort(beanNames);
-      for (String beanName : beanNames) {
-        System.out.println(beanName);
-      }
+			logger.info("Let's inspect the beans provided by Spring Boot:");
 
-    };
-  }
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
+		};
+	}
 
 }
--- springBoot/src/main/java/com/justa/springboot/service/ComplianceService.java
+++ springBoot/src/main/java/com/justa/springboot/service/ComplianceService.java
@@ -1,5 +1,9 @@
 package com.justa.springboot.service;
 
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -8,12 +12,16 @@ import org.springframework.context.annotation.PropertySource;
 @PropertySource("classpath:service.properties") 
 // will load compliance.user.list from application firstly, then load from service.properties if didn't find it 
 public class ComplianceService {
+	
+	private static final Logger log = LoggerFactory.getLogger(ComplianceService.class);
 
     private String[] userList;
     private static final String SetName = "${compliance.user.list}";
 
     // to do unit test, we inject value on constructor
     public ComplianceService(@Value(SetName) String users ) {
+    	
+    	log.info("Constructor in ComplianceService");
         if(users == null){
             String msg = String.format("Can't find %s in config.properties", SetName);
             throw new IllegalStateException(msg);
@@ -35,6 +43,11 @@ public class ComplianceService {
 
 	public String[] getUserList() {
 		return userList;
+	}
+	
+	@PostConstruct
+	public void init() {
+	   log.info("@PostConstruct in ComplianceService");
 	}
 
 
