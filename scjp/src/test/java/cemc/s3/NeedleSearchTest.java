46db44134eda401cf4cd900a189c765f1351e078
Justin.Wu <justin.wu@validusholdings.com>
2020-06-09 10:58:05 -0400

    improve needle search: add unit test cases

---
+++ scjp/src/test/java/cemc/s3/NeedleSearchTest.java
@@ -0,0 +1,63 @@
+package cemc.s3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NeedleSearchTest {
+
+	@Test
+	public void testGetTotalOcc_1_01() {
+		String needle ="aab";
+		String hays = "abacabaa";
+		int total  = NeedleSearch.getTotalOcc(needle, hays);
+		assertEquals(total, 2);
+		System.out.println(total);
+	}
+
+	@Test
+	public void testGetTotalOcc_1_02() {
+		String needle ="xxxxxxxx";
+		String hays = "nddyhsslitxxroowyofmboanirdoepueroerhrczfrpdijhxtsgzgtpzobsgricmgoqtftdctvusddsvsopztbdpbxgxcybpctclvhyuedorzrwmpzifvtnphrvwdwbovlexaxnwkgtodzlewplvurnjcedfisiapenjlhexogyeexxruupymvgsufmvpklhujnautur";
+		int total  = NeedleSearch.getTotalOcc(needle, hays);
+		assertEquals(total, 0);
+		System.out.println(total);
+	}
+
+	@Test
+	public void testGetTotalOcc_1_03() {
+		String needle ="gggggggg";
+		String hays = "pilqbbvitbcnyzadvlqlsvdxagwxkvwwqoponfhiqbynwmsfontjjvlgegnfxgggggggggtnjmwbxzhwlggggggggndwvizzkwoxfmwqtmgbwbsrnqoxfrltkcvaerokapmzuhohaggggggggwucpotxkggggggggdmsoinzgdwyavvttixlvheixjzxmiqwvzopjvmo";
+		int total  = NeedleSearch.getTotalOcc(needle, hays);
+		assertEquals(total, 1);
+		System.out.println(total);
+	}
+	
+	//@Test
+	public void testGetTotalOcc_2_04() {
+		String needle ="ffxewmweexpmiipfmfgw";
+		String hays = "fwfewpmegeiwfmipxmxffefppfemifxgmmewxwiwkmnerxcholtimemipxgewifewfmpfxfwnfemefmppxwfwiiefmwfxgtvwfiiwpegfexmwmxmffpeinpskbjazpvfwxwegieffmwxmpfmpeihrzx";
+		int total  = NeedleSearch.getTotalOcc(needle, hays);
+		assertEquals(total, 7);
+		System.out.println(total);
+	}
+	
+	@Test
+	public void testGetTotalOcc_1_05() {
+		String needle ="ggggggll";
+		String hays = "lglgggggvqglnuzjvtvitezoymuiytggggllggsamztqrfoslgggglggqtrsinggllggggmetbkhxhskglgggglglgetqwoaggglgggljjgisojggllgggggggllkrmilgggglgghgeajxqasaolgggglggdurlgggglggnqcggggllggvfmftggggggllbgwgidifqt";
+		int total  = NeedleSearch.getTotalOcc(needle, hays);
+		assertEquals(total, 10);
+		System.out.println(total);
+	}
+	
+	@Test
+	public void testGetTotalOcc_1_06() {
+		String needle ="ggguuuug";
+		String hays = "sygofjuguuggguguguuuggaxbcvutqwliguugguugetmdgguugugguguguguuugggguuuguggugcugguuugugguuguughlnnqnbgguguuugmuugguuggpuclvuugguuggnulhsuauggguuguzguugugguuggugugugugugugxbtffxbuggguugupuguugggugguguuug";
+		int total  = NeedleSearch.getTotalOcc(needle, hays);
+		assertEquals(total, 28);
+		System.out.println(total);
+	}
+
+}
