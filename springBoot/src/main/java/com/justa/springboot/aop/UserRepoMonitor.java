7e7b50f4f2f39de3b13c9db3cf313d958631a1cb
Justin.Wu <justin.wu@validusholdings.com>
2020-07-02 10:12:10 -0400

    improve @Aspect sample

--- springBoot/src/main/java/com/justa/springboot/aop/UserRepoMonitor.java
+++ springBoot/src/main/java/com/justa/springboot/aop/UserRepoMonitor.java
@@ -1,6 +1,7 @@
 package com.justa.springboot.aop;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -13,8 +14,19 @@ public class UserRepoMonitor {
 	private static final Logger logger = LoggerFactory.getLogger(UserRepoMonitor.class);
 
 	@Before("execution(* com.justa.springboot.db.*UserRepository.getUsersByNameAndPosition(..))")
+	// first * means any return type
 	public void beforeFindById(JoinPoint joinPoint) throws Throwable {
-		String methodName = joinPoint.getSignature().getName();
+		
+		Signature s = joinPoint.getSignature();
+		String methodName = s.getName();
 		logger.info(" ---> Method " + methodName + " is about to be called");
+		
+		String kind = joinPoint.getKind();
+		logger.info(" ---> kind =" + kind );
+		
+		Class<?> c = s.getDeclaringType();
+		logger.info(" ---> class =" + c.getName() );
+
+		
 	}
 }
