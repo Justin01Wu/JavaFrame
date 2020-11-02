b5f69f8bf5ff3444000d3c18e4d518b3d1bcde26
Justin.Wu <justin.wu@validusholdings.com>
2020-11-02 10:51:50 -0500

    add spring scheduler sample

--- springBoot/src/main/java/com/justa/springboot/Application.java
+++ springBoot/src/main/java/com/justa/springboot/Application.java
@@ -7,9 +7,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 // from https://spring.io/guides/gs/spring-boot/
 @SpringBootApplication(scanBasePackages = {"com.justa.springboot"})
+@EnableScheduling
 public class Application {
 
 	private static final Logger logger = LoggerFactory.getLogger(Application.class);
