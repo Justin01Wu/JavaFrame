9daad5001a947a016eff49feb3dbf81c44c8d9a8
Justin.Wu <justin.wu@validusholdings.com>
2021-06-04 10:18:16 -0400

    improve lambda sample

--- scjp/src/main/java/scjp/lambda/Lambda.java
+++ scjp/src/main/java/scjp/lambda/Lambda.java
@@ -2,6 +2,9 @@ package scjp.lambda;
 
 import java.util.ArrayList;
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 // from https://www.w3schools.com/java/java_lambda.asp
 // please also see https://www.baeldung.com/java-8-lambda-expressions-tips
@@ -16,19 +19,28 @@ interface StringFunction {
 public class Lambda {
 	public static void main(String[] args) {
 		
-		StringFunction exclaim = (s) -> s + "!";  // define a lambda function based on interface
+		StringFunction exclaim = (s) -> s + "!";  // define a lambda function based on interface, it is an simplified anonymous interface
+		// please see https://medium.com/swlh/understanding-java-8s-consumer-supplier-predicate-and-function-c1889b9423d
 		StringFunction ask = (s) -> s + "?";
 		
 		printFormatted("Hello", exclaim);
 		printFormatted("Hello", ask);
 		
 		ArrayList<Integer> numbers = new ArrayList<Integer>();
-	    numbers.add(5);
-	    numbers.add(9);
-	    numbers.add(8);
+	    numbers.add(7);
 	    numbers.add(1);
 	    Consumer<Integer> method = (n) -> { System.out.println(n); };  // built in interface for single parameter lambda function
 	    numbers.forEach( method );
+	    
+	    // Java provide those built in functional interfaces: Consumer, Supplier, Predicate and Functions
+	    Consumer<Integer> printNumber = (n) -> System.out.println("testing them together:" + n);  // we can make it simple by removing {}
+	    Predicate<Integer> greatThan5 = (n) -> n > 5;
+	    Supplier<Double> doubleSupplier1 = () -> Math.random();
+	    Function<Integer, Integer> add6 = (n) -> n + 6;
+	    
+	    numbers.stream().filter(greatThan5).map(add6).forEach(printNumber);  // now we use them together
+	     
+	    
 	}
 
 	public static void printFormatted(String str, StringFunction format) {
