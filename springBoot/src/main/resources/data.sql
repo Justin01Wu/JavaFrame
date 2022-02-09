d78ef1f33ad790474e467b4aac18474828d84bcc
justin.wu <justin.wu@validusresearch.com>
2022-02-09 11:50:39 -0500

    improve recursive query sample, upgrade spring boot version

--- springBoot/src/main/resources/data.sql
+++ springBoot/src/main/resources/data.sql
@@ -9,3 +9,9 @@ INSERT INTO user (name, email, position) VALUES
 INSERT INTO test_bean (ind_orientation, gender) VALUES
   ('L', 10),
   ('P', 20);
+  
+INSERT INTO contract2 (id, programId, name, type, eventLimit, parentId ) VALUES
+  (1, 398, 'L1A', null, 2000, null ),
+  (2, 398, 'L2A', 'C', 	3000, 1),
+  (3, 398, 'L2B', 'I', 	3500, 1),
+  (4, 398, 'L3A', 'C', 	4000, 2);
--- springBoot/src/main/resources/schema.sql
+++ springBoot/src/main/resources/schema.sql
@@ -18,3 +18,39 @@ CREATE TABLE test_bean (
   ind_orientation VARCHAR(25) NULL,
   gender int NULL
 );
+
+DROP TABLE IF EXISTS contract2;
+
+CREATE TABLE contract2 (
+  id INT PRIMARY KEY,
+  programId INT NOT NULL,
+  name VARCHAR(250) NOT NULL,
+  parentId INT,
+  type char(1),
+  eventLimit float
+);
+
+--  recursive way to get contract full name
+-- from https://stackoverflow.com/questions/10176610/how-to-show-recursive-parentid-in-a-single-column-in-sql
+DROP TABLE IF EXISTS VW_CONTRACT;
+
+CREATE VIEW VW_CONTRACT 
+AS 
+  WITH conTree (id, name, parentId, fullName) as 
+(
+  SELECT id,	name,	parentId,	name as fullName
+  FROM contract2
+  WHERE parentId IS NULL
+
+  UNION ALL
+
+  SELECT t2.id,	t2.name,	t2.parentId,           
+		 CASE  
+			WHEN t2.type ='C' THEN t2.name || ' Component OF ' || conTree.fullName  
+			WHEN t2.type ='I' THEN t2.name || ' Inure to ' || conTree.fullName  
+	        ELSE 'UNKNOWN' 
+		END AS fullName
+  FROM contract2 t2 
+     INNER JOIN conTree ON conTree.id = t2.parentid
+)
+SELECT * FROM conTree;
