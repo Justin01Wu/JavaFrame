68ae1b777a5cb67cd257e3ad116e4daaac50fc0e
justin.wu <justin.wu@validusresearch.com>
2022-06-29 10:41:45 -0400

    add jsonPath2 project

---
+++ test/jsonpath2/src/main/java/com/justa/App.java
@@ -0,0 +1,15 @@
+package com.justa;
+
+import com.jayway.jsonpath.Configuration;
+import com.jayway.jsonpath.spi.json.JacksonJsonNodeJsonProvider;
+import com.jayway.jsonpath.spi.mapper.JacksonMappingProvider;
+
+// from https://stackoverflow.com/questions/27244431/how-to-change-values-in-a-json-file-using-xpath-jsonpath-in-java
+public class App {
+	
+
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ test/jsonpath2/src/test/java/com/justa/AppTest.java
@@ -0,0 +1,43 @@
+package com.justa;
+
+import com.jayway.jsonpath.Configuration;
+import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.internal.filter.ValueNodes.JsonNode;
+import com.jayway.jsonpath.spi.json.JacksonJsonNodeJsonProvider;
+import com.jayway.jsonpath.spi.mapper.JacksonMappingProvider;
+
+import junit.framework.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest   
+{
+	private static final Configuration configuration = Configuration.builder()
+		    .jsonProvider(new JacksonJsonNodeJsonProvider())
+		    .mappingProvider(new JacksonMappingProvider())
+		    .build();
+	
+	
+	@Test
+	public void a_value_can_be_updated(){
+
+	    String originalJson = "{\n"
+	        + "\"session\":\n"
+	        + "    {\n"
+	        + "        \"name\":\"JSESSIONID\",\n"
+	        + "        \"value\":\"5864FD56A1F84D5B0233E641B5D63B52\"\n"
+	        + "    },\n"
+	        + "\"loginInfo\":\n"
+	        + "    {\n"
+	        + "        \"loginCount\":77,\n"
+	        + "        \"previousLoginTime\":\"2014-12-02T11:11:58.561+0530\"\n"
+	        + "    }\n"
+	        + "}";
+
+	    JsonNode updatedJson = JsonPath.using(configuration).parse(originalJson).set("$.session.name", "MYSESSINID").json();
+
+	    System.out.println(updatedJson.toString());
+	}
+    
+}
