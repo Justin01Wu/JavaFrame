fb66237c69ad6828a113e95aa1af4a5ed207535b
justin.wu <justin.wu@validusresearch.com>
2022-06-29 11:28:55 -0400

    update jsonPath2 project

--- test/jsonpath2/src/test/java/com/justa/AppTest.java
+++ test/jsonpath2/src/test/java/com/justa/AppTest.java
@@ -5,8 +5,13 @@ import org.junit.Test;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.jayway.jsonpath.Configuration;
 import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.internal.JsonFormatter;
 import com.jayway.jsonpath.spi.json.JacksonJsonNodeJsonProvider;
 import com.jayway.jsonpath.spi.mapper.JacksonMappingProvider;
+
+import net.minidev.json.JSONObject;
+import net.minidev.json.parser.JSONParser;
+import net.minidev.json.parser.ParseException;
 
 /**
  * Unit test for simple App.
@@ -18,11 +23,7 @@ public class AppTest
 		    .mappingProvider(new JacksonMappingProvider())
 		    .build();
 	
-	
-	@Test
-	public void a_value_can_be_updated(){
-
-	    String originalJson = "{\n"
+    private static final String originalJson = "{\n"
 	        + "\"session\":\n"
 	        + "    {\n"
 	        + "        \"name\":\"JSESSIONID\",\n"
@@ -34,10 +35,33 @@ public class AppTest
 	        + "        \"previousLoginTime\":\"2014-12-02T11:11:58.561+0530\"\n"
 	        + "    }\n"
 	        + "}";
+	
+	//@Test
+	public void a_value_can_be_updated(){
 
-	    JsonNode updatedJson = JsonPath.using(configuration).parse(originalJson).set("$.session.name", "MYSESSINID").json();
 
-	    System.out.println(updatedJson.toString());
+
+	    
+	    System.out.println(originalJson);
+	    JsonNode updatedJson = JsonPath.using(configuration).parse(originalJson)
+	    		.set("$.session.name", "__newValueA")
+	    		//.add("$.session2", "__newValueB")
+	    		.json();	    
+
+	    System.out.println(updatedJson.toPrettyString());
 	}
+	
+	@Test
+	public void a_node_can_be_updated_and_created() throws ParseException{
+		
+		System.out.println(originalJson);
+		JSONObject json = (JSONObject)new JSONParser(JSONParser.DEFAULT_PERMISSIVE_MODE).parse(originalJson);
+		JSONObject session = JsonPath.read(json, "$.session");
+		session.appendField("new__field", "new__value");
+		session.put("name", "Justin2");
+		System.out.println(JsonFormatter.prettyPrint(json.toJSONString()) );
+		
+	}
+	
     
 }
