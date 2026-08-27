d1e58f60ee61b0571c693cc1ac0eb92d633293ad
Justin Wu <justin.wu@aig.com>
2026-08-27 18:22:33 +0000

    fix some warnings

--- test/jsonpath2/src/main/java/com/justa/App.java
+++ test/jsonpath2/src/main/java/com/justa/App.java
@@ -1,9 +1,5 @@
 package com.justa;
 
-import com.jayway.jsonpath.Configuration;
-import com.jayway.jsonpath.spi.json.JacksonJsonNodeJsonProvider;
-import com.jayway.jsonpath.spi.mapper.JacksonMappingProvider;
-
 // from https://stackoverflow.com/questions/27244431/how-to-change-values-in-a-json-file-using-xpath-jsonpath-in-java
 public class App {
 	
